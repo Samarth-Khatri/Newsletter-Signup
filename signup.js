f0b51e97a33c69f26513cc46effad6750ff19f41
Samarth Khatri <samarth.khatri0000@gmail.com>
2021-10-05 02:25:18 +0530

    Corrected code

--- signup.js
+++ signup.js
@@ -40,7 +40,7 @@ app.post("/", function(req, res) {
 
   const request = https.request(url, options, function(response) {
 
-    if (response.statusCode === 20) {
+    if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html");
     } else {
       res.sendFile(__dirname + "/failure.html");
